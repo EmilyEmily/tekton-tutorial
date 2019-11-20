1f02329de6ed0f91ef15724e3b60a0f0d264b8ff
Jing Liu <liujcdl@cn.ibm.com>
2019-11-20 17:21:58 +0800

    1120change

--- src/picalc.go
+++ src/picalc.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func say(word string) string {
-    title := "[ 20191031 ] Hello world, this is"
-    return title + " " + word
+	title := "[ 20191031 ] Hello world, this is"
+	return title + " " + word
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    log.Print("Say something!")
-    fmt.Fprintf(w, "%s\n", say("BLUE1031!!!"))
+	log.Print("Say something!")
+	fmt.Fprintf(w, "%s\n", say("BLUE1120!!!"))
 }
 
 func main() {
-    log.Print("Say something started.")
+	log.Print("Say something started.")
 
-    http.HandleFunc("/", handler)   
+	http.HandleFunc("/", handler)
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
